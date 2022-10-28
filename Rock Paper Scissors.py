a3b30f338fd32ebdb22d10115b364f89481a1569
Sankalpadhikariii <115810533+Sankalpadhikariii@users.noreply.github.com>
2022-10-28 22:58:09 +0545

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,37 +1,36 @@
-import random
-choices = ["Rock", "Paper","Scissors"]
-computer = random.choice(choices)
+from random import randint
+
+#create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+#assign a random play to the computer
+computer = t[randint(0,2)]
+
+#set player to False
 player = False
-cpu =0
-user=0
-while True:
-player = input("Rock, Paper or  Scissors?").capitalize()
-    ## Conditions of Rock,Paper and Scissors
+
+while player == False:
+#set player to True
+    player = input("Rock, Paper, Scissors?")
     if player == computer:
         print("Tie!")
     elif player == "Rock":
         if computer == "Paper":
             print("You lose!", computer, "covers", player)
-            cpu+=1
         else:
             print("You win!", player, "smashes", computer)
-            player+=1
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cut", player)
-            cpu+=1
         else:
             print("You win!", player, "covers", computer)
-            player+=1
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose...", computer, "smashes", player)
-            cpu+=1
         else:
             print("You win!", player, "cut", computer)
-            player+=1
-    elif player=='End':
-        print("Final Scores:")
-        print(f"CPU:{cpu}")
-        print(f"Player:{player}")
-        break
+    else:
+        print("That's not a valid play. Check your spelling!")
+    #player was set to True, but we want it to be False so the loop continues
+    player = False
+    computer = t[randint(0,2)]
